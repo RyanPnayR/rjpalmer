7da39193ba598724d6a10f8709f64398b6e45c1b
Ryan Palmer <ryanpnayr@gmail.com>
2024-02-13 18:17:18 -0500

    Created site. Added blocking for routes to different pices of my portfolio. One to the comedy side and the other to the engineering.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import * as Card from "$lib/components/ui/card";
+  	import { goto } from '$app/navigation';
+</script>
+
+<div class="grid place-items-center grid-cols-2 gap-1 h-screen">
+<Card.Root on:click={()=>goto('/coder')}>
+  <Card.Content>
+    <p>Coder</p>
+  </Card.Content>
+</Card.Root>   
+<Card.Root on:click={()=>goto('/comedian')}>
+  <Card.Content>
+    <p>Comedian</p>
+  </Card.Content>
+</Card.Root> 
+</div>
---
+++ src/routes/comedian/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import * as Card from "$lib/components/ui/card";
+</script>
+
+<div class="grid place-items-center grid-cols-1 gap-1 h-screen">
+<Card.Root>
+  <Card.Content>
+    <p>Comedian</p>
+  </Card.Content>
+</Card.Root> 
+</div>
