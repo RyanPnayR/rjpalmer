19f29d0dc2e36d046efe31c46a86360ff4bd015d
Ryan Palmer <ryanpnayr@gmail.com>
2024-02-23 17:42:36 -0500

    Pointing site just to coder part for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,25 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
+	let x = 0;
+	let y = 0;
+	function mouseHandler(t: { currentTarget: any; clientX: any; clientY: any }) {
+		let { currentTarget: e, clientX: i, clientY: n } = t;
+		let { left: r, top: o } = e.getBoundingClientRect();
+		x = i - r;
+		y = n - o;
+	}
 </script>
 
-<slot />
+<body
+	class="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900"
+>
+	<div>
+		<div class="group/spotlight relative" role="article" on:mousemove={mouseHandler}>
+			<div
+				class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+				style="background: radial-gradient(600px at {x}px {y}px, rgba(29, 78, 216, 0.15), transparent 80%);"
+			></div>
+			<slot />
+		</div>
+	</div>
+</body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import * as Card from "$lib/components/ui/card";
   import { goto } from '$app/navigation';
+  import Header from './coder/header-section.svelte';
+	import About from './coder/about-section.svelte';
+	import Experience from './coder/expierence-section.svelte';
+	import Projects from './coder/project-section.svelte';
+	import Blog from './coder/blog-section.svelte';
+	import Footer from './coder/footer-section.svelte';
 </script>
-
-<div class="grid place-items-center grid-cols-2 gap-1 h-screen">
+<!-- <div class="grid place-items-center grid-cols-2 gap-1 h-screen">
 <Card.Root on:click={()=>goto('/coder')}>
   <Card.Content>
     <p>Coder</p>
@@ -13,5 +18,26 @@
   <Card.Content>
     <p>Comedian</p>
   </Card.Content>
-</Card.Root> 
+</Card.Root>  
+</div>-->
+
+<div
+	class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0"
+>
+	<a
+		href="#content"
+		class="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0"
+		>Skip to Content</a
+	>
+	<div class="lg:flex lg:justify-between lg:gap-4">
+		<Header />
+		<main id="content" class="pt-24 lg:w-1/2 lg:py-24">
+			<About />
+			<Experience />
+			<Projects />
+			<!-- <Blog /> -->
+			<Footer />
+		</main>
+	</div>
 </div>
+
