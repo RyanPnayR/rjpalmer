2f815e32a593273cc67b6f80adb3da33007e315e
Ryan Palmer <ryanpnayr@gmail.com>
2024-02-27 14:21:50 -0500

    Updating bio, adding skills

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,9 +35,8 @@
 			<About />
 			<Experience />
 			<Projects />
-			<!-- <Blog /> -->
+			<Blog />
 			<Footer />
 		</main>
 	</div>
 </div>
-
--- src/routes/coder/about-section.svelte
+++ src/routes/coder/about-section.svelte
@@ -1,40 +1,26 @@
 <section
-id="about"
-class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-aria-label="About me"
+	id="about"
+	class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+	aria-label="About me"
 >
-<div
-  class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
->
-  <h2 class="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
-    About
-  </h2>
-</div>
-<div>
-  <p class="mb-4">
-    The first Hello World I ever wrote was in a 9th grade elective class in <a
-    class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-    href="https://en.wikipedia.org/wiki/Racket_(programming_language)"
-    target="_blank"
-    rel="noreferrer noopener"
-    aria-label="Racket programming_language (opens in a new tab)"
-    >Racket</a>. This led to taking 3 more classes learning the fundamentals of loops, variables, code testing, and making 8 bit video games. Since then I've had the privilege of building out software for an insurance company, 4 start ups, and a lot of interesting projects in between.  
-  </p>
+	<div
+		class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+	>
+		<h2 class="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
+	</div>
+	<div>
+		<p class="mb-4">
+			Total 6+ years exp. Senior Software Engineer with a proven track record of driving customer
+			acquisition and simplifying processes. Increased onboarding rate by 33% in 1 year. Built
+			scalable backends, automated data syncs for customer support, created live call transcription,
+			and modernized web chat app with 0 downtime in 6 months.
+		</p>
 
-  <p class="mb-4">
-    Currently seeking an engaging fullstack or frontend role using a more modern stack using these prefered Languages, Frameworks, DBs: <br> 
-  </p>
-  <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
-    {#each ['React', 'Svelte', 'Next.js', 'Flutter', 'Python', 'Kotlin', 'Postgres', 'MongoDB', 'MySql', 'SQLite'] as tech}
-      <li class="mr-1.5 mt-2">
-        <div
-          class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
-        >
-          {tech}
-        </div>
-      </li>
-    {/each}
-  </ul>
-
-</div>
+		<p class="mb-4">
+			Presently in pursuit of an enriching opportunity within fullstack or frontend development,
+			harnessing modern technolody stacks to drive impactful solutions. Eager to
+			contribute my expertise in designing and implementing scalable systems, optimizing user
+			experiences, and leading collaborative efforts towards innovation and success.
+		</p>
+	</div>
 </section>
